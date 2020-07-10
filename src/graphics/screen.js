28e7062ca339da817b27842284f0901c3d8e7dae
vadinci <info@vadinci.net>
2020-07-10 10:22:03 +0200

    Added simple dynamic resizing to Screen

--- src/graphics/screen.js
+++ src/graphics/screen.js
@@ -2,23 +2,82 @@ import Dispatcher from "../core/dispatcher";
 
 let _canvas;
 
+let _targetWidth = 480;
+let _targetHeight = 640;
+
+let _resizeTaskId = null;
+
+//TODO listen for screen resize
+
 let init = function (settings) {
-	//TODO way more functionality
 	_canvas = document.createElement('canvas');
-	_canvas.width = settings.width || 480;
-	_canvas.height = settings.height || 640;
+	_targetWidth = settings.width || 480;
+	_targetHeight = settings.height || 640;
 
 	document.body.appendChild(_canvas);
+
+	_fitToScreen();
+
+	window.addEventListener('resize', _onResize, false);
+	window.addEventListener('orientationchange', _onResize, false);
 };
 
-let resize = function (width, height) {
-	_canvas.width = width;
-	_canvas.height = height;
+let _getWindowSize = function () {
+	let windowSize = {
+		width: 0,
+		height: 0
+	};
+	if (window.dapi) {
+		windowSize.width = window.dapi.getScreenSize().width;
+		windowSize.height = window.dapi.getScreenSize().height;
+	} else if (window.mraid) {
+		windowSize.width = window.mraid.getMaxSize().width;
+		windowSize.height = window.mraid.getMaxSize().height;
+	} else {
+		windowSize.width = window.innerWidth;
+		windowSize.height = window.innerHeight;
+	}
+	return windowSize;
+};
+
+let _fitToScreen = function () {
+	let windowSize = _getWindowSize();
+
+	let canvasRatio = _targetWidth / _targetHeight;
+	let screenRatio = windowSize.width / windowSize.height;
+
+	if (screenRatio > canvasRatio){	//screenRatio is wider than target ratio.
+		_canvas.width = _targetHeight * screenRatio;
+		_canvas.height = _targetHeight;
+	} else {
+		_canvas.width = _targetWidth;
+		_canvas.height = _targetWidth / screenRatio;
+	};
+
+	_canvas.style.width = windowSize.width;
+	_canvas.style.height = windowSize.height;
+
+	_resizeTaskId = null;
 
 	Screen.emit('resize', {
 		width: _canvas.width,
 		height: _canvas.height
 	});
+};
+
+let _onResize = function () {
+	// start a 100ms timeout, if interupted with a repeat event start over
+	if (_resizeTaskId != null) {
+		window.clearTimeout(_resizeTaskId);
+	}
+	_resizeTaskId = window.setTimeout(_fitToScreen, 100);
+};
+
+let resize = function (width, height) {
+	_targetWidth = width;
+	_targetHeight = height;
+
+	_fitToScreen();
 };
 
 let Screen = {
@@ -32,11 +91,11 @@ Object.defineProperties(Screen, {
 	canvas: {
 		get: () => _canvas
 	},
-	width : {
-		get : () => _canvas.width
+	width: {
+		get: () => _canvas.width
 	},
-	height : {
-		get : () => _canvas.height
+	height: {
+		get: () => _canvas.height
 	}
 });
 
